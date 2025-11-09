30f6b7095bb747f8c7e365589a5f2b7ceeabc79e
ikrar24 <mdikrar753@gmail.com>
2025-11-09 21:04:15 +0530

    fix auth error

--- Backend/src/Auth/createToken.js
+++ Backend/src/Auth/createToken.js
@@ -1,33 +1,23 @@
-import jwt from "jsonwebtoken"
-import dotenv from "dotenv"
-
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 dotenv.config();
 
-export const createToken = (req , res)=>{
+export const createToken = (req, res) => {
+  try {
+    const token = jwt.sign({ check: "ok" }, process.env.TOKEN_SECRETE, {
+      expiresIn: "2d",
+    });
 
+    res.cookie("boostViewers", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production", // Prod me true, local me false
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      maxAge: 2 * 24 * 60 * 60 * 1000,
+    });
 
-    try {
-        
-
-
-const token = jwt.sign({check:"ok"} , process.env.TOKEN_SECRETE, {expiresIn:"2d"} );
-
-
-res.cookie("boostViewers" , token , {
-    httpOnly:true,
-    secure:false, // reminder for true
-    maxAge: 2 * 24 * 60 * 60 * 1000, // 2 days
-} )
-
-res.status(201).json({message:"Token stored in cookies"})
-
-
-
-    } catch (error) {
-        console.log(error);
-        res.json({message:"Internal server error"})
-        
-    }
-
-
-}
+    res.status(201).json({ message: "Token stored in cookies" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
